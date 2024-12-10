32bb693bd6d926fe25445e50742cf2ff12f5fa8d
y.yoshimura <info.echovisionstudio@gmail.com>
2024-12-10 19:02:42 +0900

    include追加

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php include($_SERVER['DOCUMENT_ROOT']."/includes/foundation/_head.php"); ?>
+</head>
+<body>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/layout/_header.php"); ?>
+<main class="main"></main>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/layout/_footer.php"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/foundation/_script.php"); ?>
+</body>
+</html>
